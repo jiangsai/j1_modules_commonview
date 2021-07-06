b875a09e7d7ee077dc4fcac3448dcf5d888d7fe0
jyt <125839128@qq.com>
2021-07-06 15:58:46 +0800

    测试提交到哪里去
    Change-Id: I40239602f2467fd64df7f3b8bf50cb673accad26

--- app/src/main/java/com/playplan/boot/MainActivity.java
+++ app/src/main/java/com/playplan/boot/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.playplan.architecture_view.Test;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Test.test();
     }
 }
---
+++ architecture_view/src/main/java/com/playplan/architecture_view/Test.java
@@ -0,0 +1,14 @@
+package com.playplan.architecture_view;
+
+import android.util.Log;
+
+/**
+ * author : jyt
+ * time   : 2021/07/06
+ * desc   :
+ */
+public class Test {
+    public static void test() {
+        Log.e("jyt", "test");
+    }
+}
